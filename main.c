ae5fb469a60c37b3f19130a11cf5cf322a705c8f
Le Bao Trung <trunglbth1809003@fpt.edu.vn>
2018-09-26 15:46:43 +0700

    test bt phep cong

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a ;
+    int b ;
+    printf("\n moi nhap so a: ");
+    scanf( "%d", &a);
+    printf("\n moi nhap so b: ");
+    scanf( "%d", &b);
+    printf( "\n %d + %d =%d ",a,b,a+b );
+    // kết thúc chương trình
+    return 0;
+}
